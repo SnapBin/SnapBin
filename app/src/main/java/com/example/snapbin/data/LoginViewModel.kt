1c3d9d2ed0f5c1b62efbc9ad627d450228e50923
imraghavbnsal007 <raghavb025@gmail.com>
2024-02-05 14:42:35 +0000

    UI layer and State Validation

---
+++ app/src/main/java/com/example/snapbin/data/LoginViewModel.kt
@@ -0,0 +1,98 @@
+package com.example.snapbin.data
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.snapbin.data.rules.Validator
+
+class LoginViewModel: ViewModel() {
+    private val  TAG = LoginViewModel::class.simpleName
+    var registrationUIState = mutableStateOf(RegistrationUIState())
+
+    fun onEvent(event:UIEvent){
+        when(event){
+            is UIEvent.FirstNameChanged -> {
+                registrationUIState.value = registrationUIState.value.copy(
+                    firstName = event.firstName
+                )
+                validateDataWithRules()
+                printState()
+            }
+
+            is UIEvent.LastNameChanged -> {
+                registrationUIState.value = registrationUIState.value.copy(
+                    lastName = event.lastName
+                )
+                validateDataWithRules()
+                printState()
+            }
+
+            is UIEvent.EmailChanged -> {
+                registrationUIState.value = registrationUIState.value.copy(
+                    email = event.email
+                )
+                validateDataWithRules()
+                printState()
+            }
+
+            is UIEvent.PasswordChanged -> {
+                registrationUIState.value = registrationUIState.value.copy(
+                    password = event.password
+                )
+                validateDataWithRules()
+                printState()
+            }
+
+            is UIEvent.RegisterButtonClicked -> {
+                signUp()
+            }
+        }
+
+    }
+    private fun signUp() {
+        Log.d(TAG, "Inside_signUp")
+        printState()
+
+        validateDataWithRules()
+
+    }
+
+    private fun validateDataWithRules() {
+        val fNameResult = Validator.validateFirstName(
+            fName = registrationUIState.value.firstName
+        )
+
+        val lNameResult = Validator.validateLastName(
+            lName = registrationUIState.value.lastName
+        )
+
+        val emailResult = Validator.validateEmail(
+            email = registrationUIState.value.email
+        )
+        val passwordResult = Validator.validatePassword(
+            password = registrationUIState.value.password
+        )
+
+        Log.d(TAG, "INsidevalidator")
+        Log.d(TAG, "fNameResult = $fNameResult")
+        Log.d(TAG, "lNameResult = $lNameResult")
+        Log.d(TAG, "emailResult = $emailResult")
+        Log.d(TAG, "passwordResult = $passwordResult")
+
+        registrationUIState.value = registrationUIState.value.copy(
+            firstNameError = fNameResult.status,
+            lastNameError = lNameResult.status,
+            emailError = emailResult.status,
+            passwordError = passwordResult.status
+        )
+
+    }
+
+
+    private fun printState(){
+        Log.d(TAG, "Inside_printState")
+        Log.d(TAG, registrationUIState.value.toString())
+    }
+
+
+}
---
+++ app/src/main/java/com/example/snapbin/data/RegistrationUIState.kt
@@ -0,0 +1,13 @@
+package com.example.snapbin.data
+
+data class RegistrationUIState(
+    var firstName :String = "",
+    var lastName  :String = "",
+    var email  :String = "",
+    var password  :String = "",
+
+    var firstNameError: Boolean = false,
+    var lastNameError: Boolean = false,
+    var emailError: Boolean = false,
+    var passwordError: Boolean = false
+)
